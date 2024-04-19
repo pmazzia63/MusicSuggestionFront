023d935d894ed27ddf7385e4ae6a15768f51666c
Paul Mazzia <p.mazzia63@gmail.com>
2024-04-19 17:27:25 +0200

    hold way

--- src/components/FormSongInfo.js
+++ src/components/FormSongInfo.js
@@ -19,17 +19,23 @@ function FormSongInfo({ setResults }) {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        axios.post('http://13.37.217.48:8000/songs/', song)
-            .then(response => {
-                console.log('Similar Songs:', response.data);
-                alert('Similar songs found successfully!');
-                setResults(response.data)
-                // Optionally, process and display the similar songs data here
+        fetch('http://13.37.217.48:8000/register/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify()
+            })
+            .then(response => response.json()) // Convert the response data to JSON
+            .then(data => {
+                console.log('Success:', data);
+                alert(data.message);
             })
             .catch(error => {
                 console.error('Error:', error);
-                alert('Failed to find similar songs');
+                alert('Failed to register user');
             });
+        
     };
 
     return (
