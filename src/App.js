ea33afe950614f5d438940875284737b3a962438
Paul Mazzia <p.mazzia63@gmail.com>
2024-04-19 16:21:17 +0200

    remove reset

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import React, { useState, useEffect } from 'react';
 import FormUserInfo from './components/FormUserInfo';
 import FormSongInfo from './components/FormSongInfo';
 import DisplayResults from './components/DisplayResults';
-import axios from 'axios';
+// import axios from 'axios';
 
 import './App.css';
 
 function App() {
     const [results, setResults] = useState('');
 
-    // useEffect hook to run on component mount
-    useEffect(() => {
-        // Action to perform on page refresh or initial load
-        axios.post('http://13.37.217.48:8000/reset/', {})
-            .then(response => {
-                console.log('Success:', response.data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('Failed to register user');
-            });
+    // // useEffect hook to run on component mount
+    // useEffect(() => {
+    //     // Action to perform on page refresh or initial load
+    //     axios.post('http://13.37.217.48:8000/reset/', {})
+    //         .then(response => {
+    //             console.log('Success:', response.data);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error:', error);
+    //             alert('Failed to register user');
+    //         });
 
-        // You can place any logic here that you want to execute on page refresh
-    }, []); // Empty dependency array means this runs only once when the component mounts
+    //     // You can place any logic here that you want to execute on page refresh
+    // }, []); // Empty dependency array means this runs only once when the component mounts
 
     return (
         <div className="App">
